61e9c61a62b57b60e078df552b99c5b3e6e2d476
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-07-13 13:02:13 +0200

    lint

--- eea/api/coremetadata/api/services/metadata/get.py
+++ eea/api/coremetadata/api/services/metadata/get.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
-from .adapters.interfaces import ICoreMetadata
+from zope.component import adapter
+from zope.interface import Interface
+from zope.interface import implementer
+
 from plone import api
 from plone.restapi.interfaces import IExpandableElement
 from plone.restapi.services import Service
-from zope.component import adapter
-from zope.interface import implementer
-from zope.interface import Interface
+
+from .adapters.interfaces import ICoreMetadata
 
 
 @implementer(IExpandableElement)
 @adapter(Interface, Interface)
 class Metadata(object):
+    """ Expandable service factory implementation """
+
     def __init__(self, context, request):
         self.context = context.aq_explicit
         self.request = request
@@ -24,21 +28,22 @@ class Metadata(object):
                 ),
             },
         }
-        # Ignore expansion mechanism, always
-        # show expanded results
+        # Ignore expansion mechanism,
+        # always show expanded results
         #
         # if not expand:
         #     return result
 
-        adapter = ICoreMetadata(self.context)
-
-        items = adapter.render_metadata()
+        core_metadata_adapter = ICoreMetadata(self.context)
+        items = core_metadata_adapter.render_metadata()
 
         result["metadata"]["items"] = items
         return result
 
 
 class MetadataGet(Service):
+    """ @metadata endpoint implementation """
+
     def reply(self):
         service_factory = Metadata(self.context, self.request)
         return service_factory(expand=True)["metadata"]
